94f79db33db4c4ab6533931313de3e1999cee179
“kvchgtw” <kvchgtw@gmail.com>
2023-11-20 02:39:36 +0800

    add more animals in prompt

--- app/create/page.js
+++ app/create/page.js
@@ -8,7 +8,7 @@ import Link from 'next/link'
 
 
 
-const animalList = ['fox', 'dog', 'cat', 'elephant']
+const animalList = ['fox', 'dog', 'cat', 'elephant', 'pig', 'monkey', 'rabbit']
 let selectedCategorySaved = {}
 const bookCategories = [
     {
