98c586c97a17ce07b7be1bd9601221cd38188440
“kvchgtw” <kvchgtw@gmail.com>
2023-11-26 02:42:42 +0800

    add loading status

--- app/collections/[collectionId]/page.js
+++ app/collections/[collectionId]/page.js
@@ -1,44 +1,52 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import { collection, query, where, onSnapshot } from "firebase/firestore";
-import db from '../../components/database/database'; // 确保正确导入了 Firestore 数据库实例
+import db from '../../components/database/database';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import styles from '../../components/styles/Collections.module.css'; // Assuming you have a CSS module for styling
-
+import styles from '../../components/styles/Collections.module.css';
+import { useUser } from "@clerk/clerk-react";
 
 function CollectionIdPage({ params }) {
   const collectionId = params.collectionId;
-  const [images, setImages] = useState([]); // 更新状态以存储图片对象
-  const [isLoading, setIsLoading] = useState(true); // 新增加载状态标志
+  const [images, setImages] = useState([]);
+  const [allLoaded, setAllLoaded] = useState(false);
+  const { user } = useUser();
+  const userId = user?.id; // Use optional chaining
 
   useEffect(() => {
-    // 创建查询：检查 imageUrl 不为空，并且 collectionId 匹配
+    // Ensure userId is defined before proceeding
+    if (!userId) {
+      console.log('userId is not defined');
+      return;
+    }
+
     const q = query(
       collection(db, "images"),
       where("imageUrl", "!=", ""),
-      where("collectionId", "==", collectionId)
+      where("collectionId", "==", collectionId),
+      where('userId', '==', userId)
     );
 
-    // 设置 Firestore 实时监听器
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const imagesArray = [];
       querySnapshot.forEach((doc) => {
         const data = doc.data();
-        imagesArray.push({ imageUrl: data.imageUrl, itemName: data.itemName }); // 获取 imageUrl 和 itemName
+        imagesArray.push({ imageUrl: data.imageUrl, itemName: data.itemName });
       });
-      setImages(imagesArray); // 更新状态
-      setIsLoading(false); // 数据加载完毕，设置加载状态为 false
+      setImages(imagesArray);
+      console.log('update query image array: ', imagesArray);
+      loader(imagesArray);
     });
 
-    // 清理函数：组件卸载时取消监听
     return () => unsubscribe();
-  }, [collectionId]);
+  }, [collectionId, userId]);
 
-  // 加载时显示的内容
-  if (isLoading) {
-    return <div>Loading images...</div>;
+  function loader(imagesArray) {
+    if (imagesArray.length !== 0) {
+      setAllLoaded(true);
+    }
   }
 
   const settings = {
@@ -49,19 +57,28 @@ function CollectionIdPage({ params }) {
     slidesToScroll: 1
   };
 
+  // Conditional rendering based on the availability of userId
+  if (!userId) {
+    return <div>Loading user data...</div>;
+  }
+
   return (
-    <div className={styles.imageContainer}>
-      <Slider {...settings}>
-        {images.map((image, index) => (
-          <div key={index}>
-            <img className={styles.collectionImage} src={image.imageUrl} alt={image.itemName || 'Image'} />
-              {/* <div className={styles.textShadow}></div> */}
-              <div className={styles.itemNameText}>{image.itemName}</div> {/* 显示图片名称 */}
-          
-          </div>
-        ))}
-      </Slider>
-    </div>
+    <>
+      {allLoaded ? (
+        <div className={styles.imageContainer}>
+          <Slider {...settings}>
+            {images.map((image, index) => (
+              <div key={index}>
+                <img className={styles.collectionImage} src={image.imageUrl} alt={image.itemName || 'Image'} />
+                <div className={styles.itemNameText}>{image.itemName}</div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      ) : (
+        <div className={styles.loaderText}>Loading images...</div>
+      )}
+    </>
   );
 }
 
--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ import Header from './components/header' // 注意這裡改為默認導入
 import './globals.css'
 import Footer from './components/Footer'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
