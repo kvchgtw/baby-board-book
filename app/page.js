4127a7a7d9e6d87665eeeab73b1e3a4266917b91
“kvchgtw” <kvchgtw@gmail.com>
2024-01-19 13:27:41 +0800

    update landing page

--- app/page.js
+++ app/page.js
@@ -9,38 +9,40 @@ import Image from 'next/image';
 
 export default function homepage(){
   return(
-    <>
-    {/* <div className={styles.homePageBackground}> */}
-      
-      <div className={styles.homePageElementContainer}>
-        <div className={styles.indexFirstImageContainer}>
-        <Image priority={true}   sizes="100vw" src={foxRaccoonBg}  className={styles.foxRaccoonBg}   
-          style={
-            {objectFit: 'cover',}
-          }/>
-          <Image priority={true}   sizes="100vw" src={foxRaccoon}  className={styles.foxRaccoon}   
-          style={
-            {objectFit: 'cover',}
-          }/>
-        </div>
-        <div className={styles.mainElement}>
-          <div className={styles.mainElementCenter}>
-            <div className={styles.titleText}>
-            Discover the Joy of Reading Together
-            </div>
-            <div className={styles.subTitleText}>
-            AI Baby Board Books is designed for loving family moments.            </div>
-            <div className={styles.ctaBtnConatainer}>
-              <Link href='/create'>
-                <button className={styles.createCTAbtn}>Create Now</button>
-              </Link>
+    <>      
+        <div className={styles.homePageElementContainer}>
+          <div className={styles.indexFirstImageContainer}>
+            
+              <Image priority={true}   sizes="100vw" src={foxRaccoonBg}  className={styles.foxRaccoonBg}   
+                style={
+                  {objectFit: 'cover',}
+                }
+              />
+              <span>
+                <Image priority={true}   sizes="100vw" src={foxRaccoon}  className={styles.foxRaccoon}   
+                  style={
+                    {objectFit: 'cover',}
+                  }
+                />
+              </span>
+            
+          </div>
+          <div className={styles.mainElement}>
+            <div className={styles.mainElementCenter}>
+              <div className={styles.titleText}>
+              Discover the Joy of Reading Together
+              </div>
+              <div className={styles.subTitleText}>
+              AI Baby Board Books is designed for loving family moments.            </div>
+              <div className={styles.ctaBtnConatainer}>
+                <Link href='/create'>
+                  <button className={styles.createCTAbtn}>Create Now</button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    {/* </div> */}
-    <Footer />
-
+      <Footer />
     </>
   )
 }
